8705054a5d2f6fc228e7a3198aecf3d7b090eba7
Andr3xDev <andr3xdev@gmail.com>
2026-08-05 02:57:53 -0500

    fix: remove unnecesary elements & uses right format & elements

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,3 @@
-use crossterm::event;
-
-fn main() -> std::io::Result<()> {
-    ratatui::run(|mut terminal| {
-        loop {
-            terminal.draw(|frame| frame.render_widget("Hello World!", frame.area()))?;
-            if event::read()?.is_key_press() {
-                break Ok(());
-            }
-        }
-    })
+fn main() {
+    println!("Hello, world!");
 }
